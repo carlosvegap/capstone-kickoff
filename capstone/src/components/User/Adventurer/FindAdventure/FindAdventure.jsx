eae19a2b4974a254355fb05b7346013714c43e96
Carlos Vega <carlosvega@fb.com>
2022-07-14 15:25:27 -0700

    on prefix for setDisplayExperience -> onSelectDisplay

--- capstone/src/components/User/Adventurer/FindAdventure/FindAdventure.jsx
+++ capstone/src/components/User/Adventurer/FindAdventure/FindAdventure.jsx
@@ -3,22 +3,22 @@ import { useState } from 'react';
 import Map from './Map/Map';
 
 export default function FindAdventure({ isLoggedIn }) {
-  const [displayExperience, setDisplayExperience] = useState('map');
+  const [displayExperience, onSelectDisplay] = useState('map');
   return (
     <div className="findAdventure">
       <h2>Find your next adventure:</h2>
       <div className="experienceDisplayer">
-        <ExperienceViewButton value="map" displayExperience={displayExperience} setDisplayExperience={setDisplayExperience} />
-        <ExperienceViewButton value="list" displayExperience={displayExperience} setDisplayExperience={setDisplayExperience} />
+        <ExperienceViewButton value="map" displayExperience={displayExperience} onSelectDisplay={onSelectDisplay} />
+        <ExperienceViewButton value="list" displayExperience={displayExperience} onSelectDisplay={onSelectDisplay} />
       </div>
       <Map isLoggedIn={isLoggedIn} />
     </div>
   );
 }
 
-function ExperienceViewButton({ value, displayExperience, setDisplayExperience }) {
+function ExperienceViewButton({ value, displayExperience, onSelectDisplay }) {
   if (displayExperience === value) {
     return <button type="button" value={value} className="chosenDisplay" disabled>{value}</button>;
   }
-  return <button type="button" value={value} className="notChosenDisplay" onClick={() => setDisplayExperience(value)}>{value}</button>;
+  return <button type="button" value={value} className="notChosenDisplay" onClick={() => onSelectDisplay(value)}>{value}</button>;
 }
