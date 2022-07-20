2a03f137a3dcf96dcc513ce48a8c373be39ab23c
Carlos Vega <carlosvega@fb.com>
2022-07-20 16:32:12 -0700

    eliminating unnecesary catch blocks

--- back/routes/adventure.js
+++ back/routes/adventure.js
@@ -17,10 +17,6 @@ router.post('/restaurants', async(req, res) => {
   .then(function (response) {
     res.status(200);
     res.send(response.data);
-  })
-  .catch(function (error) {
-    res.status(400);
-    res.send(error)
   });
 })
 
@@ -31,27 +27,17 @@ Parse.serverURL = 'http://parseapi.back4app.com';
 router.post('/preferences/active', async(req, res) => {
   const query = new Parse.Query('UserPreference');
   query.equalTo("username", req.body.username)
-  try {
-    let currentPreferences = await query.find();
-    res.status(200);
-    res.send(currentPreferences[0].toJSON().activePreferences);
-  } catch(error) {
-    res.status(400);
-    res.send(error);
-  }
+  let currentPreferences = await query.find();
+  res.status(200);
+  res.send(currentPreferences[0].toJSON().activePreferences);
 })
 
 // ----- Get all existing Preferences -----
 router.get('/preferences/all', async(req, res) => {
   const query = new Parse.Query('Preference');
-  try {
-    let preferences = await query.find();
-    res.status(200);
-    res.send(preferences);
-  } catch(error) {
-    res.status(400);
-    res.send(error);
-  }
+  let preferences = await query.find();
+  res.status(200);
+  res.send(preferences);
 })
 
 // ----- Get all inactive preferences -----
@@ -86,12 +72,7 @@ router.post('/preferences/find', async(req, res) => {
   let preferenceInformation = []
   const query = new Parse.Query("Preference");
   let allPreferences = [];
-  try {
-    allPreferences = await query.find()
-  } catch(error) {
-    res.status(400);
-    res.send(error);
-  }
+  allPreferences = await query.find()
   preferenceInformation = allPreferences.filter((preference) => objectIdArray.includes(preference.toJSON().objectId))
   preferenceInformationJSON = preferenceInformation.map((preference) => preference.toJSON())
   res.status(200);
@@ -113,26 +94,16 @@ router.post('/preferences/update', async(req, res) => {
   const findQuery = new Parse.Query('UserPreference');
   findQuery.equalTo("username", username)
   let objectId = null;
-  try {
-    let currentPreferences = await findQuery.find();
-    objectId = currentPreferences[0].toJSON().objectId
-  } catch(error) {
-    res.status(401);
-    res.send(error);
-  }
+  let currentPreferences = await findQuery.find();
+  objectId = currentPreferences[0].toJSON().objectId
   // Update information for that user
   let updateQuery = new Parse.Object('UserPreference');
   updateQuery.set('objectId', objectId)
   if (differentPriority) updateQuery.set('prioritize', prioritize)
   if (differentActivePrefences) updateQuery.set('activePreferences', activePreferences)
-  try {
-    await updateQuery.save();
-    res.status(200);
-    res.send('Success');
-  } catch(error) {
-    res.status(402);
-    res.send(error);
-  }
+  await updateQuery.save();
+  res.status(200);
+  res.send('Success');
 })
 
 module.exports = router;
