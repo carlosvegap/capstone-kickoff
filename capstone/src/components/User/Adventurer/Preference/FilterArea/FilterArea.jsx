8e4b6b4dcab62087fa338461114a784d6bbc0355
Carlos Vega <carlosvega@fb.com>
2022-07-21 11:22:17 -0700

    refactorization work

--- capstone/src/components/User/Adventurer/Preference/FilterArea/FilterArea.jsx
+++ capstone/src/components/User/Adventurer/Preference/FilterArea/FilterArea.jsx
@@ -1,36 +1,49 @@
 import {
-  Box, Checkbox, Badge, HStack, Slider, SliderMark, SliderTrack, SliderFilledTrack,
-  Tooltip, SliderThumb, Button,
+  Box, Checkbox, Badge, HStack, Slider, SliderMark, SliderTrack,
+  SliderFilledTrack, Tooltip, SliderThumb, Button, VStack,
 } from '@chakra-ui/react';
 import { StarIcon, DeleteIcon } from '@chakra-ui/icons';
 import { useState } from 'react';
 
-export default function FilterArea({
-  id, displayText, priority, minValue, maxValue, defaultValue, step, units,
-}) {
-  const markSpace = maxValue / 5;
-  const [sliderValue, setSliderValue] = useState(0);
-  const [showToolTip, setShowTooltip] = useState(false);
-  const [showMinimumValues, setShowMinimumValues] = useState(false);
-  if (showMinimumValues === false) {
-    return (
-      <Box>
-        <HStack spacing="40px" mb="20px">
-          <Checkbox ml="45px" onChange={(e) => setShowMinimumValues(e.target.checked)} />
-          <Badge> {priority} {displayText} </Badge>
-          <Button rightIcon={<DeleteIcon />} colorScheme="red" variant="outline" alignSelf="right">Delete</Button>
-        </HStack>
-      </Box>
-    );
-  }
+// Top part of the filter options: set a minimum value for preference, read its name and delete it
+function FilterControls({ setShowMinimumValues, priority, displayText }) {
   return (
     <Box>
-      <HStack spacing="40px">
-        <Checkbox ml="45px" onChange={(e) => setShowMinimumValues(e.target.checked)} />
-        <Badge> {priority} {displayText} </Badge>
-        <Badge colorScheme="yellow"> {sliderValue} <DefineIcon units={units} />  or Higher </Badge>
-        <Button rightIcon={<DeleteIcon />} colorScheme="red" variant="outline">Delete</Button>
+      <HStack spacing="40px" mb="20px">
+        <Checkbox
+          ml="45px"
+          onChange={(e) => setShowMinimumValues(e.target.checked)}
+        />
+        <Badge> {priority} </Badge>
+        <Badge> {displayText} </Badge>
+        <Button
+          rightIcon={<DeleteIcon />}
+          colorScheme="red"
+          variant="outline"
+          alignSelf="right"
+        >
+          Delete
+        </Button>
       </HStack>
+    </Box>
+  );
+}
+
+// Slider to control the minimum value for the preference
+function SliderControl({
+  id,
+  minValue,
+  maxValue,
+  defaultValue,
+  step,
+  units,
+}) {
+  const markSpace = maxValue / 5;
+  const [sliderValue, setSliderValue] = useState(defaultValue);
+  const [showToolTip, setShowTooltip] = useState(false);
+  return (
+    <VStack mb="40px">
+      <Badge colorScheme="yellow"> {sliderValue} <DefineIcon units={units} /> or more </Badge>
       <Slider
         width="80%"
         ml="10%"
@@ -46,9 +59,19 @@ export default function FilterArea({
         onMouseEnter={() => setShowTooltip(true)}
         onMouseLeave={() => setShowTooltip(false)}
       >
-        {[markSpace, 2 * markSpace, 3 * markSpace, 4 * markSpace].map((interval) => (
-          <SliderMark value={interval} mt="1" ml="-2.5" fontSize="sm" textAlign="center">{Math.round(interval)} </SliderMark>
-        ))}
+        {[markSpace, 2 * markSpace, 3 * markSpace, 4 * markSpace, maxValue].map(
+          (interval) => (
+            <SliderMark
+              value={interval}
+              mt="1"
+              ml="-2.5"
+              fontSize="sm"
+              textAlign="center"
+            >
+              {Math.round(interval)}{' '}
+            </SliderMark>
+          ),
+        )}
         <SliderTrack>
           <SliderFilledTrack />
         </SliderTrack>
@@ -63,6 +86,41 @@ export default function FilterArea({
           <SliderThumb />
         </Tooltip>
       </Slider>
+    </VStack>
+  );
+}
+
+export default function FilterArea({
+  id,
+  displayText,
+  priority,
+  minValue,
+  maxValue,
+  defaultValue,
+  step,
+  units,
+}) {
+  const [showMinimumValues, setShowMinimumValues] = useState(false);
+  return (
+    <Box>
+      <FilterControls
+        setShowMinimumValues={setShowMinimumValues}
+        priority={priority}
+        displayText={displayText}
+      />
+      {showMinimumValues
+        && (
+        <SliderControl
+          key={id}
+          id={id}
+          minValue={minValue}
+          maxValue={maxValue}
+          defaultValue={defaultValue}
+          step={step}
+          units={units}
+        />
+        )}
+
     </Box>
   );
 }
--- capstone/src/components/User/Adventurer/Preference/FilterMenu/FilterMenu.jsx
+++ capstone/src/components/User/Adventurer/Preference/FilterMenu/FilterMenu.jsx
@@ -3,19 +3,15 @@ import {
   Menu, MenuButton, Button, MenuList, MenuItem,
 } from '@chakra-ui/react';
 
-// inactivePreferences={inactivePreferences}
-//           setInactivePreferences={setInactivePreferences}
-//           setActivePreferencesIDs={setActivePreferencesIDs}
-function handleAddition(objectId, inactivePreferences, setInactivePreferences, activePreferencesIDs, setActivePreferencesIDs) {
-  setActivePreferencesIDs([...activePreferencesIDs, objectId]);
-  setInactivePreferences(
-    inactivePreferences.filter((preferences) => preferences.objectId !== objectId),
-  );
-}
-
 export default function FilterMenu({
   inactivePreferences, setInactivePreferences, activePreferencesIDs, setActivePreferencesIDs,
 }) {
+  function handleAddition(objectId) {
+    setActivePreferencesIDs([...activePreferencesIDs, objectId]);
+    setInactivePreferences(
+      inactivePreferences.filter((preferences) => preferences.objectId !== objectId),
+    );
+  }
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<ChevronDownIcon />} width="1000px">
@@ -25,8 +21,7 @@ export default function FilterMenu({
         {inactivePreferences.map((preference) => (
           <MenuItem
             key={preference.objectId}
-            value={preference.objectId}
-            onClick={(e) => handleAddition(e.target.value, inactivePreferences, setInactivePreferences, activePreferencesIDs, setActivePreferencesIDs)}
+            onClick={() => handleAddition(preference.objectId)}
           >
             {preference.displayText}
           </MenuItem>
