fafc470eb77d5e83d5e05af753e50b82be3813d2
Carlos Vega <carlosvega@fb.com>
2022-07-26 15:10:13 -0700

    solving merge conflict

--- capstone/src/components/User/Experience/Experience.jsx
+++ capstone/src/components/User/Experience/Experience.jsx
@@ -1,37 +1,16 @@
 import { useNavigate, useParams } from 'react-router-dom';
-<<<<<<< HEAD
-import { useEffect, useState, useMemo, useContext } from 'react';
-=======
 import { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
->>>>>>> main
 import UserContext from '../../../Contexts/UserContext';
 import Header from '../Header/Header';
 import RegisterExperience from './RegisterExperience/RegisterExperience';
 import Feedback from './Feedback/Feedback';
 
-<<<<<<< HEAD
-export default function Experience({ setIsLoggedIn, isLoggedIn }) {
-  // Get user's information
-  const {
-    firstName,
-    lastName,
-    username,
-    age,
-    userType,
-    createdAt,
-    updatedAt,
-    ACL,
-    objectId,
-    experienceId,
-  } = useContext(UserContext);
-=======
 async function getExperienceInfo(username) {
   const values = { username };
   const baseURL = process.env.REACT_APP_BASE_URL;
   return axios.post(`${baseURL}/experience/info`, values);
 }
->>>>>>> main
 
 export default function Experience({ setIsLoggedIn, isLoggedIn }) {
   const { firstName, username, userType } = useContext(UserContext);
@@ -76,15 +55,11 @@ export default function Experience({ setIsLoggedIn, isLoggedIn }) {
     return (
       <>
         <Header userType={userType} onLogOutClick={setIsLoggedIn} />
-<<<<<<< HEAD
-        <RegisterExperience />
-        <Feedback />
-=======
         <RegisterExperience
           experienceValues={experienceData}
           setExperienceValues={setExperienceData}
         />
->>>>>>> main
+        <Feedback />
       </>
     );
   }
